fc51ba2201847fbb0d89f6e764c34564c85ea969
Ryan Pickett <ryan@hdpe.me>
2018-02-25 23:38:48 +0000

    Add uniq constraint to schema

--- internal/mailer/schema/0001_initial_schema.up.sql
+++ internal/mailer/schema/0001_initial_schema.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE recipients (
   id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   email VARCHAR(254) NOT NULL,
   status VARCHAR(32) NOT NULL,
-  last_modified TIMESTAMP NULL
+  last_modified TIMESTAMP NULL,
+  UNIQUE KEY uq_recipients_email (email)
 );
